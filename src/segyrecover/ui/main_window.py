ef79c6b26c0255b57aeff8b9e922edb5af058feb
Alejandro Pertuz <apertuz@ucm.es>
2025-03-31 14:37:24 +0200

    updates

--- src/segyrecover/ui/main_window.py
+++ src/segyrecover/ui/main_window.py
@@ -5,8 +5,8 @@ import os
 import json
 from PySide6.QtGui import QFont, QAction
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (
-    QVBoxLayout, QLabel, QPushButton, QMessageBox, 
+from PySide6.QtWidgets import (QApplication, QStatusBar, QProgressBar, 
+                               QVBoxLayout, QLabel, QPushButton, QMessageBox, 
     QWidget, QTextEdit, QStyle, QDialog, QFileDialog, QMainWindow
 )
 
@@ -15,7 +15,7 @@ import appdirs
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 
-from .status_bar import ProgressStatusBar
+
 from .help_dialogs import HelpDialog, FirstRunDialog, AboutDialog
 from .image_viewer import ImageLoader
 from .workflow import load_image, input_parameters, select_area, initialize
@@ -24,6 +24,59 @@ from ..utils.console_utils import (
     section_header, info_message, initialize_log_file, close_log_file
 )
 
+
+class ProgressStatusBar(QStatusBar):
+    """Status bar with integrated progress bar."""
+
+    def __init__(self, parent=None):
+        """Initialize the progress status bar."""
+        super().__init__(parent)
+        
+        # Create progress bar
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setVisible(False)
+        self.progress_bar.setMaximumHeight(15)
+        self.progress_bar.setMaximumWidth(200)
+        
+        # Create cancel button
+        self.cancel_button = QPushButton()
+        self.cancel_button.setIcon(self.style().standardIcon(QStyle.SP_DialogCancelButton))
+        self.cancel_button.setVisible(False)
+        self.cancel_button.clicked.connect(self.cancel)
+        
+        # Add widgets to status bar
+        self.addPermanentWidget(self.progress_bar)
+        self.addPermanentWidget(self.cancel_button)
+        
+        self._canceled = False
+        
+    def start(self, title, maximum):
+        self._canceled = False
+        self.showMessage(title)
+        self.progress_bar.setMaximum(maximum)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setVisible(True)
+        self.cancel_button.setVisible(True)
+        QApplication.processEvents()
+        
+    def update(self, value, message=None):
+        if message:
+            self.showMessage(message)
+        self.progress_bar.setValue(value)
+        QApplication.processEvents()
+        
+    def finish(self):
+        self.clearMessage()
+        self.progress_bar.setVisible(False)
+        self.cancel_button.setVisible(False)
+    
+    def wasCanceled(self):
+        """Check if the operation was canceled."""
+        return self._canceled
+    
+    def cancel(self):
+        """Cancel the current operation."""
+        self._canceled = True
 
 class SegyRecover(QMainWindow):
     """Main application widget for SEGYRecover."""
