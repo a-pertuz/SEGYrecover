4eb400e46ccb3058b282e502924191a92628d29b
Alejandro Pertuz <apertuz@ucm.es>
2025-04-03 12:46:18 +0200

    Bump version: 1.1.2 → 1.1.3

--- src/segyrecover/ui/help_dialogs.py
+++ src/segyrecover/ui/help_dialogs.py
@@ -30,7 +30,7 @@ class AboutDialog(QDialog):
         title.setFont(QFont("Arial", 16, QFont.Bold))
         
         # Version and copyright info
-        __version__ = '1.1.2'
+        __version__ = '1.1.3'
         version = QLabel(f"Version {__version__}")
         version.setAlignment(Qt.AlignCenter)
         
